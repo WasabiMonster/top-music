4028c29c3c816c78675e22b1ff064fa1784eccf2
Patrick Wilson <patrickwilson.tv@gmail.com>
2020-08-26 20:25:24 -0700

    Cool colors

---
+++ Top Music/Top Music/Classes/Extensions/UIColor+TopMusicAdditions.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+TopMusicAdditions.swift
+//  Top Music
+//
+//  Created by Patrick Wilson on 8/26/20.
+//  Copyright © 2020 Etechitronica. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var offWhite: UIColor {
+        return UIColor(white: 247.0/255.0, alpha: 1.0)
+    }
+    
+    static var offBlackSlightlyTransparent: UIColor {
+        return UIColor(white: 8.0/255.0, alpha: 0.85)
+    }
+    
+    static var nikeFootball: UIColor {
+        return UIColor(red: 80/255, green: 72/255, blue: 71/255, alpha: 1.0)
+    }
+        
+    static var oregonDucksGreen: UIColor {
+        return UIColor(red: 0/255, green: 79/255, blue: 57/255, alpha: 1.0)
+    }
+    
+    static var groovyPink: UIColor {
+        return UIColor(red: 251/255, green: 35/255, blue: 95/255, alpha: 1.0)
+    }
+    
+}
