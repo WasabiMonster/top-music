1936409f951f49d1a3e86cdf0324f48151b7c482
Patrick Wilson <patrickwilson.tv@gmail.com>
2020-08-30 21:19:02 -0700

    AlbumFeedRequestTests ready and passing

--- Top Music/Top MusicTests/AlbumFeedRequestTests.swift
+++ Top Music/Top MusicTests/AlbumFeedRequestTests.swift
@@ -17,10 +17,9 @@ class AlbumFeedRequestTests: XCTestCase {
         super.setUp()
         self.mockURLSession = MockURLSession()
         self.mockApiManager = MockApiManager(urlSession: self.mockURLSession)
-        
     }
     
-    func testMakeStoreListRequest() {
+    func testMakeFeedRequest() {
         
         self.mockURLSession.nextData = loadTestData(fromFile: "feed", fileExtension: ".json")
         
@@ -40,53 +39,30 @@ class AlbumFeedRequestTests: XCTestCase {
                 XCTAssertNotNil(feedResponse)
                 
                 // check the first level properties
-                print("*083020* \(type(of: self)), \(#function) |> \(feedResponse.title)")
-                print("*083020* \(type(of: self)), \(#function) |> \(feedResponse.author)")
-                print("*083020* \(type(of: self)), \(#function) |> \(feedResponse.id)")
-                print("*083020* \(type(of: self)), \(#function) |> \(feedResponse.results?.count)")
-                
-                /*
-                let title: String
-                let author: AlbumAuthorModel
-                let id: String
-                let results: [AlbumModel]?
-                */
-                
-                // let list = storeListResponse.lists.first
-                
-                
-                /* XCTAssertEqual(list?.headerTitle, "New Arrivals")
-                XCTAssertEqual(list?.listType, "new")
-                XCTAssertEqual(list?.deepLinkDict["notif_type"] as? String, "search")
-                XCTAssertEqual(list?.items.count, 12)
-                
-                // check the first item
-                let item = list?.items.first
-                XCTAssertEqual(item?.ID, "8207574")
-                XCTAssertEqual(item?.name, "Strawberry Print Phone Ring")
-                XCTAssertEqual(item?.url, URL("https://www.forever21.com/us/shop/Catalog/Product/F21/ACC/1000352881"))
-                XCTAssertEqual(item?.desktopUrl, URL("https://www.doteshopping.com/shop/items/8207574-strawberry-print-phone-ring"))
-                XCTAssertEqual(item?.msrpInDollars, 5.90)
-                XCTAssertEqual(item?.priceInDollars, 5.90)
-                XCTAssertEqual(item?.coinValue, 1000)
-                XCTAssertEqual(item?.score, 0)
-                XCTAssertEqual(item?.storeID, "10")
-                XCTAssertEqual(item?.allSizes, [])
-                XCTAssertEqual(item?.isSoldOut, false)
-                XCTAssertEqual(item?.badges, [])
-                XCTAssertEqual(item?.imageInfo?.imageUrl, URL("https://d3e1z3hxo3xobu.cloudfront.net/api/uploads/image/file/93405965/thumb_00352881-01.jpg"))
-                XCTAssertEqual(item?.imageInfo?.imageWidth, 243)
-                XCTAssertEqual(item?.imageInfo?.imageHeight, 364)
-                XCTAssertEqual(item?.imageInfo?.ID, "93405965")
-                XCTAssertEqual(item?.numOfColors, 1)
-                XCTAssertEqual(item?.loggedStoreCategoryID, "10279")
-                XCTAssertEqual(item?.loggedDoteCategoryID, "3")
-                XCTAssertEqual(item?.isNew, true)
-                XCTAssertEqual(item?.isSeen, false)
-                XCTAssertEqual(item?.isFavorite, false)
-                XCTAssertEqual(item?.favoriteListsIDs, ["1", "2"])
-                XCTAssertEqual(item?.isPurchased, false)
-                XCTAssertEqual(item?.analyticsDict["item_id"] as? Int, 8207574) */
+                XCTAssertEqual(feedResponse.title, "Top Albums")
+                XCTAssertEqual(feedResponse.author.name, "iTunes Store")
+                XCTAssertEqual(feedResponse.author.uri, "http://wwww.apple.com/us/itunes/")
+                XCTAssertEqual(feedResponse.id, "https://rss.itunes.apple.com/api/v1/us/itunes-music/top-albums/all/100/explicit.json")
+                XCTAssertEqual(feedResponse.results?.count, 100)
+
+                // check the first result album
+                guard let album = feedResponse.results?.first else {
+                    XCTFail("First Album should have a value")
+                    return
+                }
+
+                XCTAssertEqual(album.name, "King's Disease")
+                XCTAssertEqual(album.artistName, "Nas")
+                XCTAssertEqual(album.id, "1528092359")
+                XCTAssertEqual(album.artworkUrl, "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/14/4a/8f/144a8f2c-c723-2413-291b-ba2fe2819456/20UMGIM71728.rgb.jpg/200x200bb.png")
+
+                let formatter = DateFormatter.yyyyMMdd
+                XCTAssertEqual(album.releaseDate, formatter.date(from: "2020-08-21"))
+
+                XCTAssertEqual(album.copyright, "℗ 2020 Mass Appeal")
+                XCTAssertEqual(album.genres.count, 2)
+                XCTAssertEqual(album.url, "https://music.apple.com/us/album/kings-disease/1528092359?app=itunes")
+                XCTAssertEqual(album.imageLoadStatus, .start)
             }
             expectation.fulfill()
         }
